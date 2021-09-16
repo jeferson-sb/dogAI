95e915b6160df22467290787f32039c9a88561fb
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-09-15 22:35:07 -0300

    feat: add routing and pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from './routes'
+
+function App() {
+  return (
+    <Router>
+      <Routes />
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+import Logo from 'components/Logo'
+import FormUpload from 'components/FormUpload/FormUpload'
+
+import styles from './Dashboard.module.css'
+
+function Dashboard() {
+  return (
+    <main className={styles.container}>
+      <Link to="/" className={styles.link}>
+        <Logo className={styles.dashboardLogo} />
+      </Link>
+      <FormUpload />
+    </main>
+  )
+}
+
+export default Dashboard
